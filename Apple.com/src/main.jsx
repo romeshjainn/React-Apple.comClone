7bdc67625255f0293032272fb65009aced5c64e7
Romesh Jain <romeshjainofficial@gmail.com>
2023-09-10 14:24:38 +0530

    update

--- Apple.com/src/main.jsx
+++ Apple.com/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from "react-dom/client";
 import { Header } from './Header'; // Import the named export
+import { SectionA } from './SectionA';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <SectionA />
   </React.StrictMode>
 );
